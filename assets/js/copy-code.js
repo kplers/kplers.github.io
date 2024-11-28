c597dca7d6e6b461a3b99ed74c23aa4fc32b04dd
kplers <judge3791@kaist.ac.kr>
2024-11-28 23:30:45 +0900

    Revert "f"
    This reverts commit 9075103eea88b5be18e44f8e1e82bfd94ab01f38.

--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -1,25 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const codeBlocks = document.querySelectorAll('pre code');
-    
-    codeBlocks.forEach(function(codeBlock) {
-      const container = codeBlock.parentNode;
-      const wrapper = document.createElement('div');
-      wrapper.className = 'code-wrapper';
-      container.parentNode.insertBefore(wrapper, container);
-      wrapper.appendChild(container);
-      
-      const copyButton = document.createElement('button');
-      copyButton.className = 'copy-button';
-      copyButton.innerHTML = '<i class="fas fa-copy"></i>';
-      
-      copyButton.addEventListener('click', function() {
-        navigator.clipboard.writeText(codeBlock.textContent);
-        copyButton.innerHTML = '<i class="fas fa-check"></i>';
-        setTimeout(() => {
-          copyButton.innerHTML = '<i class="fas fa-copy"></i>';
-        }, 2000);
-      });
-      
-      wrapper.appendChild(copyButton);
+
+    codeBlocks.forEach(function (codeBlock) {
+        const container = codeBlock.parentNode;
+        const copyButton = document.createElement('button');
+        copyButton.className = 'copy-button';
+        copyButton.innerHTML = '<i class="fas fa-copy"></i>';
+
+        copyButton.addEventListener('click', function () {
+            navigator.clipboard.writeText(codeBlock.textContent);
+            copyButton.innerHTML = '<i class="fas fa-check"></i>';
+            setTimeout(() => {
+                copyButton.innerHTML = '<i class="fas fa-copy"></i>';
+            }, 2000);
+        });
+
+        container.style.position = 'relative';
+        container.appendChild(copyButton);
     });
-  });
+});
